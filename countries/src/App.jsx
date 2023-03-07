3cfdef3e549212c26dfd99ea431745be9beea1cd
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2023-03-07 17:22:47 +0100

    vite and sass initialization, navbar completed

---
+++ countries/src/App.jsx
@@ -0,0 +1,21 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import "./sass/_main.scss";
+
+
+function App() {
+ 
+  return (
+    <div className="App"> 
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </Router>      
+    </div>
+  )
+}
+
+export default App
---
+++ countries/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav className='navbar'>
+        <p className="slogan">Where in the world?</p>
+        <div className="mode-switch"><ion-icon name="moon-outline"></ion-icon><span className="mode-text">Dark Mode</span></div>
+    </nav>
+  )
+}
+
+export default Navbar
